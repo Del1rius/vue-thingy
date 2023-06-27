c3bce38535ec1b9a8cb9ddc272a93cf9368ef204
Del1rius <timothy.aidan.barry@gmail.com>
2023-06-27 16:42:52 +0200

    something

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <div>
+    <NavBarComp/>
+  </div>
   <router-view/>
 </template>
-
+<script>
+import NavBarComp from './components/NavBar-Comp.vue';
+export default {
+  components: {NavBarComp}
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
---
+++ src/components/NavBar-Comp.vue
@@ -0,0 +1,35 @@
+<template >
+    <div>
+      <router-link to="/">Home</router-link>  
+      <router-link to="/about">About</router-link>
+      <router-link to="/products">Products</router-link>
+      <router-link to="/contact">Contact</router-link>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+ div{
+    background-color: black;
+    margin: 10px;
+    padding: 30px;
+ }
+
+ a {
+    text-decoration: none;
+    color: crimson;
+    text-align: center;
+    font-size: 25px;
+    padding: 20px;
+    margin: 20px;
+ }   
+
+ a:hover {
+    background-color: crimson;
+    color: lavenderblush;
+    border-radius: 20px;
+ }
+</style>
